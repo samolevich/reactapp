f673f4f65a7fa6e11404c742cfd404f297388078
samolevich <samolevich@gmail.com>
2020-05-11 21:40:30 +0300

    add map for repeated Item component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export class App extends Component {
   changeTitle = (arg) => {
     this.setState({title: arg})
   }
-
+ 
   inputChange = (event) => {
     this.setState({title: event.target.value})
   }
@@ -25,9 +25,7 @@ export class App extends Component {
         <input type="text" onChange={this.inputChange} value={this.state.title} />
         <h1>{this.state.title}</h1>
         <button onClick={() => this.changeTitle('main')}>Change title</button>
-        <Item name={this.state.items[0].name} number={this.state.items[0].number} changeTitle={this.changeTitle} />
-        <Item name={this.state.items[1].name} number={this.state.items[1].number} changeTitle={this.changeTitle} />
-        <Item name={this.state.items[2].name} number={this.state.items[2].number} changeTitle={this.changeTitle} />
+        { this.state.items.map(item => <Item name={item.name} number={item.number} changeTitle={this.changeTitle} />) }
       </div>
     )
   }
